db8af89830e048c5732f550df89a71372c0925d7
Mii4a <syoutacancer123>
2020-04-16 00:11:53 +0900

    learn reactive system, computed, methods, watch, setTimeout, :class, :style, directive

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -6,9 +6,25 @@
     <router-link to="/page2">Page2</router-link>
     <router-view></router-view>
     <p>現在{{ number }}回clickされています</p>
-    <button v-on:click="countUp(2)"></button>
-    <p v-on:mousemove="mouseLocation(3, $event)">マウスを載せてください</p>
+    <button @click="countUp(2)"></button>
+    <p v-on:mousemove="mouseLocation(10, $event)">
+      <a v-on:click.prevent.stop href="https://google.com">Google</a>
+      <span v-on:mousemove.stop>反応しないでください</span>
+      マウスを載せてください
+    </p>
     <p>X:{{x}}, Y:{{y}}</p>
+    <input type="text" v-on:keyup.enter="myAlert" />
+    <input type="text" v-model="command" />
+    <h1>{{ command }}</h1>
+    <p>{{ counter }}</p>
+    <button @click="counter += 1">+1</button>
+
+    <h1 :class="classObject">Hello</h1>
+    <button @click="isActive = !isActive">切り替え</button>
+    <h1 :class="[{red: isActive}, bg]">Hello</h1>
+    <p>{{ lessThanThree }}</p>
+
+    <h1 :style="[baseObject, styleObject]">Hello</h1>
     <Footer msg="This is common footer rendered by vue's single file components"></Footer>
   </div>
 </template>
@@ -29,19 +45,61 @@ export default {
   },
   data: function() {
     return {
+      command: "こんにちは",
       number: 0,
+      counter: 0,
       x: 0,
       y: 0,
+      event: "click",
+      isActive: true,
+      color: "red",
+      bg: "bg-blue",
+      textColor: "red",
+      bgColor: "blue",
+      styleObject: {
+        color: "red",
+        "background-color": "blue"
+      },
+      baseObject: {
+        'font-size': '4rem'
+      }
+    };
+  },
+  computed: {
+    lessThanThree: function() {
+      return this.counter > 3 ? "4以上" : "3以下";
+    },
+    classObject: function() {
+      return {
+        red: this.isActive,
+        "bg-blue": !this.isActive
+      };
+    }
+  },
+  watch: {
+    counter: function() {
+      const vm = this;
+      setTimeout(function() {
+        vm.counter = 0;
+      }, 3000);
     }
   },
   methods: {
     countUp: function(times) {
-      this.number += 1 * times
+      this.number += 1 * times;
     },
-    mouseLocation: function(divideNumber, event){
-      console.log(event);
+    mouseLocation: function(divideNumber, event) {
       this.x = event.clientX / divideNumber;
       this.y = event.clientY / divideNumber;
+    },
+    noEvent: function(event) {
+      event.stopPropagation();
+    },
+    myAlert() {
+      alert("アラート");
+    },
+    lessThanThreeMethod: function() {
+      return this.counter ? "4以上" : "3以下";
     }
   }
 };
@@ -55,5 +113,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.red {
+  color: red;
+}
+.bg-blue {
+  background-color: blue;
 }
 </style>
